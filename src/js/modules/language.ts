4cba16c8931f680fbcfc465b6aa7ff3b9b5ee5b0
SvayOff <marsiul2001@gmail.com>
2025-01-08 21:31:40 +0200

    change language html & updated the language logic

--- src/js/modules/language.ts
+++ src/js/modules/language.ts
@@ -1,36 +1,63 @@
 export const languageControle = (): void => {
 	const language = document.querySelector<HTMLDivElement>('.header__lang')!;
+	const languageTitle = document.querySelector<HTMLButtonElement>('.header__lang-title_btn');
 
-	const langDefault = {
+	const langDefault: LangType = {
 		langName: 'Українська',
 		lang: 'ua'
 	};
 
-	language.addEventListener('click', (e: Event) => onClickLanguage(e));
+	type LangType = {
+		langName: string;
+		lang: string;
+	};
 
-	const onClickLanguage = (e: Event) => {
+	const onClickLanguage = (e: Event): void => {
 		const target = e.target as HTMLElement;
 
 		toggleLanguagePopup(target);
+
+		choiseLanguage(target);
 	}
 
 	const toggleLanguagePopup = (target: HTMLElement): void => {
-		if (target.classList.contains('header__lang-title')) {
+		if (target.classList.contains('header__lang-title_btn')) {
 			language.classList.toggle('show');
 		}
 	}
 
-	document.addEventListener('click', (e: Event) => {
+
+	const clickOutsideLanguage = (e: Event): void => {
 		const target = e.target as HTMLElement;
 
 		if (target && !target.closest('.header__lang')) {
-			language.classList.remove('show');
+			closeLanguagePopup();
 		};
-	});
+	}
 
-	const choiseLanguage = (target: HTMLElement) => {
-		if (target.classList.contains('header__lang-')) {
-			
+	const choiseLanguage = (target: HTMLElement): void => {
+		if (target.classList.contains('header__lang-item_btn') && target.dataset.lang) {
+			langDefault.langName = target.innerText;
+			langDefault.lang = target.dataset.lang;
+
+			setDefaultLanguage();
+			closeLanguagePopup();
 		};
 	};
+
+	const setDefaultLanguage = (): void => {
+		if (languageTitle) {
+			languageTitle.textContent = langDefault.langName;
+			languageTitle.dataset.lang = langDefault.lang;
+		}
+	}
+
+	const closeLanguagePopup = (): void => {
+		language.classList.remove('show');
+	};
+
+	setDefaultLanguage();
+
+	language.addEventListener('click', (e: Event) => onClickLanguage(e));
+	document.addEventListener('click', (e: Event) => clickOutsideLanguage(e));
 };
